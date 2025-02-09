a43d1c04c7bb005c3a5037a7b21f1d706f62fb56
Jarl <jarl13.work@gmail.com>
2025-02-09 19:06:13 +0800

    modify: currency converter api to select option

--- src/lib/CurrencyConverter.svelte
+++ src/lib/CurrencyConverter.svelte
@@ -1,28 +1,41 @@
 <script>
-  const options = [
-    { value: 1, label: "USD" },
-    { value: 56, label: "PHP" },
-    { value: 1.59, label: "AUD" },
-  ];
+  const fetchCurrency = (async () => {
+    const response = await fetch(
+      "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json"
+    );
+    const data = await response.json();
+    return data;
+  })();
+
   let amountFrom = $state(0);
   let amountTo = $state();
+  let selectedCurrencyFrom = $state();
+  let selectedCurrencyTo = $state();
 </script>
 
 <main>
-  <select>
-    {#each options as option}
-      <option value={option.value}>{option.label}</option>
-    {/each}
-  </select>
   From
+  {#await fetchCurrency then data}
+    <select bind:value={selectedCurrencyFrom} id="selectedCurrencyFrom">
+      {#each Object.entries(data) as [code, name]}
+        <option value={code}>{name} ({code})</option>
+      {/each}
+    </select>
+  {:catch error}
+    <p>An error occurred!</p>
+  {/await}
   <input type="number" id="amountFrom" min="0" bind:value={amountFrom} />
   To
-  <select>
-    {#each options as option}
-      <option value={option.value}>{option.label}</option>
-    {/each}
-  </select>
 
+  {#await fetchCurrency then data}
+    <select bind:value={selectedCurrencyTo} id="selectedCurrencyTo">
+      {#each Object.entries(data) as [code, name]}
+        <option value={code}>{name} ({code})</option>
+      {/each}
+    </select>
+  {:catch error}
+    <p>An error occurred!</p>
+  {/await}
   <button> Convert </button>
   <input type="number" id="amountTo" min="0" bind:value={amountTo} readonly />
 </main>
